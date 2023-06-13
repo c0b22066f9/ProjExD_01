e49e99ae309676ded11b4145f1ed415c5204aba8
Yuki Saito <c0b22066f9@edu.teu.ac.jp>
2023-06-13 17:29:13 +0900

    第6問目

--- pygame_image.py
+++ pygame_image.py
@@ -1,23 +1,36 @@
 import sys
 import pygame as pg
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    screen = pg.display.set_mode((800, 600))
+    #bg_img_rct= bg_img.get_rect()
+    #bg_img_rct.center = 400,300
+    
+    
+    #img = pg.image.load("ex01/fig/3.png")
+
+
+    
+    clock  = pg.time.Clock()
+    
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 142, 1.0)]
     
-
     tmr = 0
     while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        for event in pg.event.get():
+
+            if event.type == pg.QUIT: return
+        x = tmr%1600
+        screen.blit(bg_img,[-x,0])
+        screen.blit(bg_img,[1600-x,0])
+        screen.blit(kk_imgs[tmr%2], [300, 200])
         pg.display.update()
+        
         tmr += 1        
         clock.tick(10)
 if __name__ == "__main__":
